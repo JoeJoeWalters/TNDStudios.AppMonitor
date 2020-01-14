b3534c37216a7d88bee3f1136b2ea8d975070038
TNDStudios <info@thenakeddeveloper.com>
2020-01-14 01:01:40 +0000

    Documenting and a few tweeks on the MonitorClient

--- Client/MonitorClient.cs
+++ Client/MonitorClient.cs
@@ -5,40 +5,62 @@ using System.Threading.Tasks;
 
 namespace TNDStudios.AppMonitor.Client
 {
+    /// <summary>
+    /// Client for .Net Core but written via Standard Library to handle trasmission of metrics,
+    /// heartbeats, errors etc. to the central hub
+    /// </summary>
     public class MonitorClient
     {
+        /// <summary>
+        /// The hub connection once it has been established
+        /// </summary>
         private HubConnection connection;
+
+        /// <summary>
+        /// The name of the application that this client is representing
+        /// </summary>
         private String applicationName;
 
-        public MonitorClient(String applicationName, String uri)
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="applicationName">The name of the host application</param>
+        /// <param name="uri">The uri of the hub to transmit to</param>
+        public MonitorClient(String applicationName, String serviceUri)
         {
             this.applicationName = applicationName;
             connection = new HubConnectionBuilder()
-                .WithUrl(new Uri(uri))
+                .WithUrl(new Uri(serviceUri))
                 .WithAutomaticReconnect(new MonitorClientRetryPolicy())
                 .Build();
         }
 
+        /// <summary>
+        /// Connect to the remote service to start communication
+        /// </summary>
+        /// <returns>Status of the connection as a Boolean</returns>
         public Boolean Connect()
         {
-            Int32 attempts = 5;
-            CancellationToken token = new CancellationToken();
+            Int32 attempts = 5; // Limit the retry attempts
+            CancellationToken token = new CancellationToken(); // Provide a fresh cancellation token to use
 
             // Keep trying to until we can start or the token is canceled.
             while (true)
             {
                 try
                 {
+                    // Attempt the connection, associated errors from this will be trapped individually
                     connection.StartAsync(token).Wait();
                     return true;
                 }
                 catch when (token.IsCancellationRequested)
                 {
+                    // Total cancellation from the service we are connecting to
                     return false;
                 }
                 catch
                 {
-                    // Failed to connect, trying again in 5000 ms.
+                    // Failed to connect, but not cancelled, try again in 5000 ms.
                     attempts--;
                     if (0 == attempts) return false;
                     Task.Delay(5000);
@@ -46,8 +68,14 @@ namespace TNDStudios.AppMonitor.Client
             }
         }
 
-        public Boolean Heartbeat()
+        /// <summary>
+        /// Send heartbeat information to the service (tell the service that the system is alive
+        /// still)
+        /// </summary>
+        /// <returns>If the heartbeat was sent</returns>
+        public Boolean SendHeartbeat()
         {
+            // Is the hub connected to the client?
             if (connection.State == HubConnectionState.Connected)
             {
                 try
@@ -55,7 +83,6 @@ namespace TNDStudios.AppMonitor.Client
                     Int32 randomSeconds = (Int32)(2.0 * 60.0 * (new Random().NextDouble()));
                     DateTime nextRunTime = DateTime.UtcNow.AddSeconds(randomSeconds);
                     connection.InvokeAsync("SendHeartbeat", applicationName, nextRunTime).Wait();
-                    Console.WriteLine($"Sent Heartbeat - Next Expected Run is {nextRunTime.ToString()}");
                 }
                 catch
                 {
@@ -68,14 +95,19 @@ namespace TNDStudios.AppMonitor.Client
             return true;
         }
 
-        public Boolean Error(String error)
+        /// <summary>
+        /// Transmit an error from the application to the hub
+        /// </summary>
+        /// <param name="error">The string representation of the error</param>
+        /// <returns>If the error was sent to the hub</returns>
+        public Boolean SendError(String error)
         {
+            // Is the hub connected to the client?
             if (connection.State == HubConnectionState.Connected)
             {
                 try
                 {
                     connection.InvokeAsync("SendError", applicationName, error).Wait();
-                    Console.WriteLine("Sent Error");
                 }
                 catch
                 {
@@ -88,14 +120,20 @@ namespace TNDStudios.AppMonitor.Client
             return true;
         }
 
-        public Boolean Send(String property, Int32 metric)
+        /// <summary>
+        /// Send a metric to the hub for this application
+        /// </summary>
+        /// <param name="property">The "name" of the metric property</param>
+        /// <param name="metric">The value of the metric</param>
+        /// <returns></returns>
+        public Boolean SendMetric(String property, Int32 metric)
         {
+            // Is the hub connected to the client?
             if (connection.State == HubConnectionState.Connected)
             {
                 try
                 {
                     connection.InvokeAsync("SendMetric", applicationName, property, metric.ToString()).Wait();
-                    Console.WriteLine("Sent Metric");
                 }
                 catch
                 {
--- Core/Controllers/TelemetryControllerBase.cs
+++ Core/Controllers/TelemetryControllerBase.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace TNDStudios.AppMonitor.Core.Controllers
 {
     [Route("api/[controller]")]
